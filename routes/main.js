896b52c612b45a50010ba7b522fea0df0006514e
Din S Ho <dinsho@Din-S-M1.local>
2022-09-28 22:37:05 -0700

    second commit

---
+++ routes/main.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const authController = require('../controllers/auth')
+const homeController = require('../controllers/home')
+const auth = require('../middleware/auth')
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
+
+router.get('/', homeController.getIndex)
+router.get('/login', authController.getLogin)
+router.post('/logout', authController.postLogin)
+router.get('/logout', authController.logout)
+router.get('/signup', authController.getSignup)
+router.post('/signup', authController.postSignup)
+
+module.exports = router
